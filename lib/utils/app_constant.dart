4c03d9566ab80f4f6cc626b8635f35aec8449c5d
nhokpy123 <phin7921@gmail.com>
2024-11-30 08:57:12 +0700

    giua_ky_app_flutter

--- lib/utils/app_constant.dart
+++ lib/utils/app_constant.dart
@@ -2,7 +2,8 @@ class AppConstants {
   static const String APP_NAME = "DBFood";
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = "http://172.16.67.209:3030";
+  static const String BASE_URL = "http://localhost:3030";//muon' thay' anh? trong database thi`
+  // doi? localhost thanh` idv4 cua? wifi nha thay`
   static const String POPULAR_PRODUCT_URI = "/api/products/allpopular";
   static const String RECOMMEND_PRODUCT_URI = "/api/recommends/testrecommended";
   static const String UPLOAD_URL = "/uploads/";
